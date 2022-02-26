c676b50c804f1141edabb5e2fcd8c252758792a8
Feroze Khan <87601912+ferozemewati@users.noreply.github.com>
2022-02-26 21:45:16 +0530

    By myself

---
+++ StarPattern.java
@@ -0,0 +1,139 @@
+// Print triangle Stars pattern
+
+/*
+public class OneLineStar {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+//  print half diamond of stars using for loop and if else
+
+/*
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=7; j++){
+                if (j>=4-i && j<=2+i) {
+                    System.out.print(" * ");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+
+//
+//public class StarPattern {
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 5; j++) {
+//                if (j >= 4 - i && j <= 2 + i) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+
+/*
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1; i<=4; i++){
+            for (int j=1; j<=4; j++){
+                if (j<=i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print("=");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+
+/*
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1; i<5; i++){
+            for (int j=2*(5-i); j>=1; j--){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*
+
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1; i<=3; i++){
+            for (int j=2*(5-i); j>=1; j--){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+/*
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i=1; i<5; i++){
+            for (int j=2*(5-i); j>=1; j--){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
+// Print triangle Stars pattern
+
+/*
+public class OneLineStar {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+
